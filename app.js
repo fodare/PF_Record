ceff3bea7c38e86dad307e71ad8ff1c5b69708b4
fodare <folorunsodamilare007@hotmail.com>
2021-05-08 14:24:52 +0200

    Create new database collection with name balances, to track the sum of all amount saved

--- app.js
+++ app.js
@@ -19,29 +19,27 @@ mongoose.connect("mongodb://localhost:27017/moneyDB", {
 });
 
 ////////// DB Schema //////////
+
+// Schema for the savings database
 const trnxDeatails = {
    date: String,
    amount: Number,
-   balance: Number,
+};
+
+// Schema for the final balance database
+const finalBalance = {
+   ID: Number,
+   date: String,
+   final_amount: Number,
 };
 
 // Create model or collection in the database
+
+// savings collection
 const saved = mongoose.model("Saving", trnxDeatails);
 
-// // Starting Balance
-// const defaultBalance = new saved({
-//     date:"2021-03-13",
-//     amount:0,
-//     balance:3000
-// });
-
-// saved.insertMany([defaultBalance], function(err){
-//     if(err){
-//         console.log(err);
-//     } else {
-//         console.log("Balance initiated!");
-//     }
-// });
+// Balance Collection
+const trackBalance = mongoose.model("balance", finalBalance);
 
 /************** GET routes **************/
 app.get("/", function (req, res) {
@@ -58,6 +56,8 @@ app.get("/support", function (req, res) {
 
 /************** POST routes **************/
 app.post("/", function (req, res) {
+   // create a new data from the submitted form
+   // Then write the new collected data to the Database
    const newSaving = new saved({
       date: req.body.day,
       amount: req.body.money,
@@ -70,3 +70,57 @@ app.post("/", function (req, res) {
       }
    });
 });
+
+const oldFinalBalance = trackBalance.findOne(
+   { ID: 1 },
+   function (err, foundData) {
+      var oldAmount = foundData.final_amount;
+      console.log(`The final amount in the db is: ${foundData.final_amount}`);
+   }
+);
+
+// function updateBalance() {
+//    var newAmount = req.body.money;
+//    var addedAmount = newAmount + oldFinalBalance.oldAmount;
+//    console.log(addedAmount);
+//    return addedAmount;
+// }
+// const newBalance = trackBalance.updateOne(
+//    { ID: 1 },
+//    { $set: { final_amount: updateBalance() } },
+//    function (err) {
+//       if (!err) {
+//          console.log("Final amount updated");
+//       } else {
+//          console.log(err);
+//       }
+//    }
+// );
+
+// // Starting Balance
+// const defaultBalance = new saved({
+//     date:"2021-03-13",
+//     amount:0,
+//     balance:3000
+// });
+
+// saved.insertMany([defaultBalance], function(err){
+//     if(err){
+//         console.log(err);
+//     } else {
+//         console.log("Balance initiated!");
+//     }
+// });
+
+// Initiating the final balance collection
+// const balanceed = new trackBalance({
+//    date: req.body.day,
+//    final_amount: req.body.money,
+// });
+// balanceed.save(function (err) {
+//    if (!err) {
+//       console.log("final balance Updated");
+//    } else {
+//       console.log(err);
+//    }
+// });
